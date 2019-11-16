2a1f316765b51e64d6fc6609c2a519ca174f2cc4
Raicu Bogdan <raicubogdan15@gmail.com>
2019-11-16 20:17:43 +0200

    added weather display function

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# car-app
-A Python Car App
+The famous 1st Group of SDA Academy proudly presents:
+
+                              #              THE CAR-APP Project
+
+The projects goal is none less than changing the world. In this revolutionary project The Tribe of The First will help
+You to...
+
+## We have a class, called Car, with the following attributes:
+-attribute1
+-attribute2
+-attribute3
+
+ ## and methods:
+- method 1
+- method 2
+- method 3
---
+++ car_app.py
@@ -0,0 +1,23 @@
+import weather
+
+class Car:
+	def __init__(self, name, color, wheels):
+		self.name = name
+		self.color = color
+		self.wheels = wheels
+		self.accelerate = ''
+		self.engine = ''
+
+	def car(self):
+		print("Car name: ", self.name)
+		print("Car color: ", self.color)
+		print("Car engine: ",self.engine)
+
+# Our dashboard screen displays the weather!
+# You can find 'w_details' in the module we imported
+
+	def w_display(self):
+		print(f"Let's see how the weather looks today:\n\n{weather.w_details}") 
+
+car = Car('x', 'y', 'z')
+car.w_display()
