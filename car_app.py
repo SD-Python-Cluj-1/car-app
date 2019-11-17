306fa1fd29cf9b22b37f397e160589e5df7b33b2
Daniel Muncaciu <danieldfgro2000@gmail.com>
2019-11-17 07:18:08 +0200

    Atribute

--- car_app.py
+++ car_app.py
@@ -1,3 +1,12 @@
+from geopy.geocoders import Nominatim
+
 class Car:
     pass
+    def __init__(self, geolocator):
 
+        def gps(self, location):
+            self.geolocator = Nominatim()
+            location = self.geolocator.geocode("175 5th Avenue NYC")
+            print(location.address)
+            print((location.latitude, location.longitude))
+            return location
